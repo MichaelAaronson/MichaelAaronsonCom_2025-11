c45cfe73b306db84fd4e0f6b6b6c405ebe9f46f9
Michael Aaronson <Michael@MichaelAaronson.com>
2025-12-25 09:11:04 +1300

    Added Person table. Recreated DbContext

--- Website/Models/Person.cs
+++ Website/Models/Person.cs
@@ -1,6 +1,23 @@
-﻿namespace Website.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Website.Models;
+
+public partial class Person
 {
-    public class Person
-    {
-    }
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? LastNme { get; set; }
+
+    public string? Company { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Notes { get; set; }
+
+    public string? ImageFilename { get; set; }
 }
