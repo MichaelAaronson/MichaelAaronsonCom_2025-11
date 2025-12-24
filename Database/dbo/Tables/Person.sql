2370e0d50d7c3e04037146ae7ae0258461a59290
Michael Aaronson <Michael@MichaelAaronson.com>
2025-12-24 18:57:10 +1300

    Created Person table Some comments added to some files

---
+++ Database/dbo/Tables/Person.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastNme] NVARCHAR(50) NULL, 
+    [Company] NVARCHAR(100) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [Phone] NVARCHAR(50) NULL, 
+    [Notes] NVARCHAR(MAX) NULL, 
+    [ImageFilename] NVARCHAR(100) NULL
+)
