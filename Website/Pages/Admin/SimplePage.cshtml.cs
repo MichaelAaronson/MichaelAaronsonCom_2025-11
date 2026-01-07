ef320d79664b0aa426dce42ee444711684fbc838
Michael Aaronson <Michael@MichaelAaronson.com>
2026-01-07 17:29:51 +1300

    Removed: class class="footer" from the footer Overlap removed.

---
+++ Website/Pages/Admin/SimplePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages.Admin
+{
+    public class SimplePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
