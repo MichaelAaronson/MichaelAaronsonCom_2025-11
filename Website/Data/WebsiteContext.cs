522f03157919524940303e44c95b210ccb0ed149
Michael Aaronson <Michael@MichaelAaronson.com>
2026-04-12 18:48:25 +1200

    WebsiteContext: for Image

--- Website/Data/WebsiteContext.cs
+++ Website/Data/WebsiteContext.cs
@@ -47,11 +47,13 @@ namespace Website.Data
             {
                 // Composite primary key
                 entity.HasKey(jds => new { jds.JobDetailId, jds.JobSkillId });
+
                 // Relationship to JobDetail
                 entity.HasOne(jds => jds.JobDetail)
                       .WithMany(jd => jd.JobDetailSkills)
                       .HasForeignKey(jds => jds.JobDetailId)
                       .OnDelete(DeleteBehavior.Cascade);
+
                 // Relationship to JobSkill
                 entity.HasOne(jds => jds.JobSkill)
                       .WithMany(js => js.JobDetailSkills)
@@ -59,24 +61,33 @@ namespace Website.Data
                       .OnDelete(DeleteBehavior.Cascade);
             });
 
-            //modelBuilder.Entity<JobDetailSkill>()
-            //    .HasKey(jds => new { jds.JobDetailId, jds.JobSkillId });
+            // Configure many-to-many relationship between Person and Image
+            modelBuilder.Entity<PersonImage>(entity =>
+            {
+                // Composite primary key
+                entity.HasKey(pi => new { pi.PersonId, pi.ImageId });
 
-            //// Configure Group table
-            //modelBuilder.Entity<Group>(entity =>
-            //{
-            //    entity.ToTable("Group");
-            //    entity.Property(e => e.Name).HasMaxLength(100).IsRequired();
-            //    entity.Property(e => e.Description).HasMaxLength(500);
-            //});
-        }
+                // Relationship to Person
+                entity.HasOne(pi => pi.Person)
+                    .WithMany(p => p.PersonImages)
+                    .HasForeignKey(pi => pi.PersonId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                // Relationship to Image
+                entity.HasOne(pi => pi.Image)
+                    .WithMany(i => i.PersonImages)
+                    .HasForeignKey(pi => pi.ImageId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+            }
+
         public DbSet<Website.Models.JobDetail> JobDetail { get; set; } = default!;
         public DbSet<Website.Models.JobSkill> JobSkill { get; set; } = default!;
         public DbSet<Website.Models.Goal> Goal { get; set; } = default!;
         public DbSet<Website.Models.Domain> Domain { get; set; } = default!;
         public DbSet<Website.Models.Project> Project { get; set; } = default!;
         public DbSet<Website.Models.Step> Step { get; set; } = default!;
-
-
+        public DbSet<Website.Models.Image> Image { get; set; } = default!;
+        public DbSet<Website.Models.PersonImage> PersonImage { get; set; } = default!;
     }
 }
